05078c700062117ab555d55e682032aa3119ff3f
Alerander436 <borchmudryov@outlook.com>
2025-11-09 11:45:00 -0800

    feat: create React application entry point and basic UI structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Persona Shard</h1>
+        <p>Encrypted Mental Health Survey DApp</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
